30533737892300e1499672fdfd210ef64e45bfa3
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-03 22:36:42 -0500

    router issue

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Footer from './components/footer';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename='/'>
+      <BrowserRouter>
       <Navbar className='navbar' expand="lg">
         <Container>
           <Navbar.Brand href="#home">Hector Guevara</Navbar.Brand>
@@ -26,9 +26,9 @@ function App() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/portfolio">About Me</Nav.Link>
-              <Nav.Link href="/portfolio/portfolio">Portfolio</Nav.Link>
-              <Nav.Link href="/portfolio/contact">Contact</Nav.Link>
-              <Nav.Link href="/portfolio/resume">Resume</Nav.Link>
+              <Nav.Link href="/portfolio/#/portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="/portfolio/#/contact">Contact</Nav.Link>
+              <Nav.Link href="/portfolio/#/resume">Resume</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
