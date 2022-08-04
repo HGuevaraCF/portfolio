36dcb16727c3e27807b01a37d8d2b76b4d0130bd
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-03 21:49:19 -0500

    app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {
+  Route,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+import AboutMe from './components/about';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Resume from './components/resume';
+import Footer from './components/footer';
+// import { AboutMe, Portfolio, Contact, Resume, Footer } from './components'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar className='navbar' expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Hector Guevara</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">About Me</Nav.Link>
+              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/resume">Resume</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className='content'>
+        <Routes>
+        <Route exact path="/" element={<AboutMe/>} />
+          <Route path="/portfolio" element={<Portfolio/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/resume" element={<Resume/>} />
+        </Routes>
+      </div>
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
