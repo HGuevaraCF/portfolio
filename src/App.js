fd1f31152dbb7f1bf1fb8d5df4ca4c084a2b5107
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-06 11:43:08 -0500

    router issue

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ function App() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/">About Me</Nav.Link>
-              <Nav.Link href="/work">Portfolio</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
-              <Nav.Link href="/resume">Resume</Nav.Link>
+              <Nav.Link href="/portfolio/work">Portfolio</Nav.Link>
+              <Nav.Link href="/portfolio/contact">Contact</Nav.Link>
+              <Nav.Link href="/portfolio/resume">Resume</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
