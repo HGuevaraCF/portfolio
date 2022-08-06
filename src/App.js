2661145c2cb9652db0779954a7a1d646f8c103de
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-06 13:06:25 -0500

    router test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-// import {
-//   Route,
-//   Routes,
-//   BrowserRouter,
-//   Router
-// } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import AboutMe from './components/about';
 import Portfolio from './components/portfolio';
 import Contact from './components/contact';
 import Resume from './components/resume';
 import Footer from './components/footer';
 import Header from './components/header';
-// import { AboutMe, Portfolio, Contact, Resume, Footer } from './components'
-
 
 function App() {
   return (
