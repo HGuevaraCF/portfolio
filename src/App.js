9506bcabea590d2298f5eb57665f2b54619d867d
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-06 11:50:31 -0500

    router issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import {
   Route,
   Routes,
-  BrowserRouter
+  BrowserRouter,
+  Router,
+  Switch
 } from "react-router-dom";
 import AboutMe from './components/about';
 import Portfolio from './components/portfolio';
@@ -19,30 +21,32 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar className='navbar' expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Hector Guevara</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/portfolio">About Me</Nav.Link>
-              <Nav.Link href="/portfolio/work">Portfolio</Nav.Link>
-              <Nav.Link href="/portfolio/contact">Contact</Nav.Link>
-              <Nav.Link href="/portfolio/resume">Resume</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <div className='content'>
-        <Routes>
-          <Route path="/portfolio" element={<AboutMe/>} />
-          <Route path="/portfolio/work" element={<Portfolio/>} />
-          <Route path="/portfolio/contact" element={<Contact/>} />
-          <Route path="/portfolio/resume" element={<Resume/>} />
-        </Routes>
-      </div>
+        <Navbar className='navbar' expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Hector Guevara</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/portfolio">About Me</Nav.Link>
+                <Nav.Link href="/portfolio/work">Portfolio</Nav.Link>
+                <Nav.Link href="/portfolio/contact">Contact</Nav.Link>
+                <Nav.Link href="/portfolio/resume">Resume</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div className='content'>
+          <Router>
+            <Switch>
+              <Route path="/portfolio" element={<AboutMe />} />
+              <Route path="/portfolio/work" element={<Portfolio />} />
+              <Route path="/portfolio/contact" element={<Contact />} />
+              <Route path="/portfolio/resume" element={<Resume />} />
+            </Switch>
+          </Router>
+        </div>
       </BrowserRouter>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
