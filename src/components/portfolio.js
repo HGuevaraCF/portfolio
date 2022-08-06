2fe3658164a987b53a33a28df6a603e7a9bdeb40
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-06 11:59:47 -0500

    router issue

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -10,21 +10,22 @@ import wolfinance from '../utils/img/wolfinance.png';
 
 function Portfolio() {
     return (
-      <Container style={{
-        textAlign: 'justify'
-      }}>
-        <h1>Portfolio</h1>
-        <Row style={{
-          justifyContent: 'center'
-        }}>
-          <Project route="https://glacial-inlet-53631.herokuapp.com/" repo="https://github.com/HGuevaraCF/NoteTaker" imgroute={noteTaker} name="Note Taker"/>
-          <Project route="https://aeklf.github.io/NurseXpress/" repo="https://github.com/HGuevaraCF/NurseXpress" imgroute={nurseXpress} name="NurseXpress"/>
-          <Project route="https://hguevaracf.github.io/password-generator/" repo="https://github.com/HGuevaraCF/password-generator" imgroute={passwordGenerator} name="Password Generator"/>
-          <Project route="https://hguevaracf.github.io/code_quiz/" repo="https://github.com/HGuevaraCF/code_quiz" imgroute={quiz} name="Code Quiz"/>
-          <Project route="https://hguevaracf.github.io/weatherDashboard/" repo="https://github.com/HGuevaraCF/weatherDashboard" imgroute={weatherDashboard} name="Weather Dashboard"/>
-          <Project route="https://hguevaracf.github.io/proyecto1/" repo="https://github.com/HGuevaraCF/proyecto1" imgroute={wolfinance} name="Wolfinance"/>
-        </Row>
-      </Container>
+      // <Container style={{
+      //   textAlign: 'justify'
+      // }}>
+      //   <h1>Portfolio</h1>
+      //   <Row style={{
+      //     justifyContent: 'center'
+      //   }}>
+      //     <Project route="https://glacial-inlet-53631.herokuapp.com/" repo="https://github.com/HGuevaraCF/NoteTaker" imgroute={noteTaker} name="Note Taker"/>
+      //     <Project route="https://aeklf.github.io/NurseXpress/" repo="https://github.com/HGuevaraCF/NurseXpress" imgroute={nurseXpress} name="NurseXpress"/>
+      //     <Project route="https://hguevaracf.github.io/password-generator/" repo="https://github.com/HGuevaraCF/password-generator" imgroute={passwordGenerator} name="Password Generator"/>
+      //     <Project route="https://hguevaracf.github.io/code_quiz/" repo="https://github.com/HGuevaraCF/code_quiz" imgroute={quiz} name="Code Quiz"/>
+      //     <Project route="https://hguevaracf.github.io/weatherDashboard/" repo="https://github.com/HGuevaraCF/weatherDashboard" imgroute={weatherDashboard} name="Weather Dashboard"/>
+      //     <Project route="https://hguevaracf.github.io/proyecto1/" repo="https://github.com/HGuevaraCF/proyecto1" imgroute={wolfinance} name="Wolfinance"/>
+      //   </Row>
+      // </Container>
+      <p>Hola</p>
     )
 }
 
